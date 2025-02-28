c5bc914fd9cd51f100e81a55a30eae5a205313b2
hwpy <photodvfusr1@gmail.com>
2025-02-28 23:59:47 +1000

    добавлено отображение описания коммитов в gitsigns

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -17,4 +17,40 @@ return {
     "L3MON4D3/LuaSnip",
     dependencies = { "rafamadriz/friendly-snippets" },
   },
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup({
+        -- signs = {
+        --   add          = { text = '+' },
+        --   change       = { text = '~' },
+        --   delete       = { text = '_' },
+        --   topdelete    = { text = '‾' },
+        --   changedelete = { text = '~' },
+        -- },
+        current_line_blame = true,  -- Включить отображение информации о коммите в текущей строке
+        current_line_blame_opts = {
+          virt_text = true,
+          virt_text_pos = 'eol',  -- Отображать информацию в конце строки
+          delay = 1000,           -- Задержка перед отображением (в миллисекундах)
+          ignore_whitespace = false,
+        },
+        on_attach = function(bufnr)
+          -- Ключевые mapping'и (опционально)
+          local gs = package.loaded.gitsigns
+          vim.keymap.set('n', ']c', function()
+            if vim.wo.diff then return ']c' end
+            vim.schedule(function() gs.next_hunk() end)
+            return '<Ignore>'
+          end, { expr = true, buffer = bufnr })
+
+          vim.keymap.set('n', '[c', function()
+            if vim.wo.diff then return '[c' end
+            vim.schedule(function() gs.prev_hunk() end)
+            return '<Ignore>'
+          end, { expr = true, buffer = bufnr })
+        end,
+      })
+    end,
+  },
 }
