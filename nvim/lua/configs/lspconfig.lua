53ff53cd4e3757e91c25ac3426ed0cf868b9d725
hwpy <photodvfusr1@gmail.com>
2025-04-08 19:01:44 +1000

    добавлены LSP для установки в mason

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "lua-language-server", "pyright", "ruff" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
