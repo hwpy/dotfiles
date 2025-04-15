cbc586c5034e3ec269337c66da61c47902b9554d
hwpy <photodvfusr1@gmail.com>
2025-04-15 11:45:57 +1000

    добавлено удаление ^M для вставки из Windows добавлен плагин для копирования в системный буфер tmux-yank

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -7,12 +7,25 @@ require "nvchad.options"
 
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*",
-  command = ":%s/\\s\\+$//e",
+  callback = function()
+    vim.cmd([[%s/\s\+$//e]])
+    vim.opt.fileformat = "unix"
+  end
 })
 
 -- Преобразовывать CRLF -> LF при вставке
 vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function()
     vim.fn.setreg('"', vim.fn.substitute(vim.fn.getreg('"'), '\r\n', '\n', 'g'))
+  end
+})
+
+-- Убираем ^M (Carriage Return) после вставки текста из буфера обмена
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    local reg = vim.fn.getreg('"')  -- Получаем текст из системного буфера
+    -- Заменяем все символы \r на пустую строку (удаляем ^M)
+    local fixed_reg = vim.fn.substitute(reg, '\r', '', 'g')
+    vim.fn.setreg('"', fixed_reg)  -- Возвращаем исправленный текст в системный буфер
   end
 })
