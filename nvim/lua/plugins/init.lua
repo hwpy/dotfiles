88b6e4707fa052a43841ca303d51109d6bd67fd8
hwpy <photodvfusr1@gmail.com>
2025-04-18 01:17:56 +1000

    добавлен Telescope cmdline

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -26,8 +26,14 @@ return {
   -- цвета для telestope
   {
     "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
+    dependencies = { "nvim-lua/plenary.nvim", 'jonarrien/telescope-cmdline.nvim' },
+    keys = {
+      { 'Q', '<cmd>Telescope cmdline<cr>', desc = 'Cmdline' },
+      { '<leader><leader>', '<cmd>Telescope cmdline<cr>', desc = 'Cmdline' },
+    },
+    config = function(_, opts)
+      require("telescope").setup(opts)
+      require("telescope").load_extension('cmdline')
       vim.schedule(function()
         vim.api.nvim_set_hl(0, "TelescopeSelection", {
           bg = "#746DFE",
