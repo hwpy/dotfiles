a4038c77f9929c0c26db241cddb559223c022df6
hwpy <photodvfusr1@gmail.com>
2025-11-08 16:58:27 +1000

    fix: отключил smear_cursor

--- nvim/.config/nvim/lua/plugins/smear_cursor.lua
+++ nvim/.config/nvim/lua/plugins/smear_cursor.lua
@@ -1,15 +1,15 @@
 return {
-  {
-    "sphamba/smear-cursor.nvim",
-    opts = {
-      stiffness = 0.5,
-      trailing_stiffness = 0.5,
-      matrix_pixel_threshold = 0.5,
-    },
-    event = "VeryLazy",  -- Отложенная загрузка для оптимизации
-    config = function(_, opts)
-      require("smear_cursor").setup(opts)
-    end,
-  },
+  -- {
+  --   "sphamba/smear-cursor.nvim",
+  --   opts = {
+  --     stiffness = 0.5,
+  --     trailing_stiffness = 0.5,
+  --     matrix_pixel_threshold = 0.5,
+  --   },
+  --   event = "VeryLazy",  -- Отложенная загрузка для оптимизации
+  --   config = function(_, opts)
+  --     require("smear_cursor").setup(opts)
+  --   end,
+  -- },
 }
 
