99be390ac47648e3dc1d3e6dc5a1d3a2063a1773
hwpy <photodvfusr1@gmail.com>
2026-05-25 18:04:54 +1000

    feat(colors): tokyo_dark схема, унификация цветов tmux/rofi/yazi/picom/alacritty/btop/opencode, миграция обоев в .config

--- nvim/.config/nvim/lua/chadrc.lua
+++ nvim/.config/nvim/lua/chadrc.lua
@@ -6,7 +6,7 @@
 local M = {}
 
 M.base46 = {
-    theme = "onedark",
+    theme = "tokyonight",
   transparency = true,
   -- настройки конкретно для statusline theme = "vscode_colored"
   hl_override = {
