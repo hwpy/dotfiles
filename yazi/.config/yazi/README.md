f85b5e63b7b694daf70c06f83d837d90f90c9092
hwpy <photodvfusr1@gmail.com>
2026-07-14 09:48:34 +1000

    feat(yazi): добавлен плагин bunny

---
+++ yazi/.config/yazi/README.md
@@ -0,0 +1,1 @@
+ya pkg add stelcodes/bunny
--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -11,3 +11,27 @@ function Linemode:size_and_mtime()
   local size = self._file:size()
   return string.format("%s %s", size and ya.readable_size(size) or "-", time)
 end
+
+require("bunny"):setup({
+  hops = {
+    { key = "/",          path = "/",                                                  },
+    { key = "t",          path = "/tmp",                                               },
+    { key = "~",          path = "~",                        desc = "Home"             },
+    { key = "m",          path = "~/Music",                  desc = "Music"            },
+    { key = "d",          path = "~/dotfiles",               desc = "dotfiles"         },
+    { key = "c",          path = "~/.config",                desc = "Config files"     },
+    { key = { "D", "c" }, path = "~/Documents",              desc = "Documents"        },
+    { key = { "D", "w" }, path = "~/Downloads",              desc = "Downloads"        },
+    { key = { "l", "s" }, path = "~/.local/share",           desc = "Local share"      },
+    { key = { "l", "b" }, path = "~/.local/bin",             desc = "Local bin"        },
+    { key = { "l", "t" }, path = "~/.local/state",           desc = "Local state"      },
+    { key = { "g", "l" }, path = "~/Documents/repos/GitLab", desc = "GitLab projects"  },
+    { key = { "g", "h" }, path = "~/Documents/repos/GitHub", desc = "GitHub projects"  },
+    -- key and path attributes are required, desc is optional
+  },
+  desc_strategy = "path", -- If desc isn't present, use "path" or "filename", default is "path"
+  ephemeral = true, -- Enable ephemeral hops, default is true
+  tabs = true, -- Enable tab hops, default is true
+  notify = false, -- Notify after hopping, default is false
+  fuzzy_cmd = "fzf", -- Fuzzy searching command, default is "fzf"
+})
