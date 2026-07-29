08b4d46d55bb306ba19f6361ba7341171b2ba175
hwpy <photodvfusr1@gmail.com>
2026-07-29 18:48:13 +1000

    feat(nvim): отключен dashboard-nvim

--- nvim/.config/nvim/lua/plugins/dashboard-nvim.lua
+++ nvim/.config/nvim/lua/plugins/dashboard-nvim.lua
@@ -1,64 +1,64 @@
 return {
-  'nvimdev/dashboard-nvim',
-  event = 'VimEnter',
-  config = function()
-    require('dashboard').setup {
-      theme = 'doom',
-      config = {
-        header = {
-          '',
-          '╭──────────────────╮',
-          '│       NeoVim    │',
-          '╰──────────────────╯',
-          '',
-        },
-        center = {
-          {
-            icon = '  ',
-            desc = 'Find file',
-            key = 'f',
-            key_format = ' %s',
-            action = 'Telescope find_files',
-          },
-          {
-            icon = '  ',
-            desc = 'Live grep',
-            key = 'g',
-            key_format = ' %s',
-            action = 'Telescope live_grep',
-          },
-          {
-            icon = '  ',
-            desc = 'Recent files',
-            key = 'r',
-            key_format = ' %s',
-            action = 'Telescope oldfiles',
-          },
-          {
-            icon = '󰈔  ',
-            desc = 'New file',
-            key = 'n',
-            key_format = ' %s',
-            action = 'ene',
-          },
-          {
-            icon = '  ',
-            desc = 'Dotfiles',
-            key = 'd',
-            key_format = ' %s',
-            action = 'lua require("telescope.builtin").find_files({ cwd = vim.fn.expand("~/dotfiles") })',
-          },
-          {
-            icon = '󰈆  ',
-            desc = 'Quit',
-            key = 'q',
-            key_format = ' %s',
-            action = 'quit',
-          },
-        },
-        footer = {},
-      },
-    }
-  end,
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  -- 'nvimdev/dashboard-nvim',
+  -- event = 'VimEnter',
+  -- config = function()
+  --   require('dashboard').setup {
+  --     theme = 'doom',
+  --     config = {
+  --       header = {
+  --         '',
+  --         '╭──────────────────╮',
+  --         '│       NeoVim    │',
+  --         '╰──────────────────╯',
+  --         '',
+  --       },
+  --       center = {
+  --         {
+  --           icon = '  ',
+  --           desc = 'Find file',
+  --           key = 'f',
+  --           key_format = ' %s',
+  --           action = 'Telescope find_files',
+  --         },
+  --         {
+  --           icon = '  ',
+  --           desc = 'Live grep',
+  --           key = 'g',
+  --           key_format = ' %s',
+  --           action = 'Telescope live_grep',
+  --         },
+  --         {
+  --           icon = '  ',
+  --           desc = 'Recent files',
+  --           key = 'r',
+  --           key_format = ' %s',
+  --           action = 'Telescope oldfiles',
+  --         },
+  --         {
+  --           icon = '󰈔  ',
+  --           desc = 'New file',
+  --           key = 'n',
+  --           key_format = ' %s',
+  --           action = 'ene',
+  --         },
+  --         {
+  --           icon = '  ',
+  --           desc = 'Dotfiles',
+  --           key = 'd',
+  --           key_format = ' %s',
+  --           action = 'lua require("telescope.builtin").find_files({ cwd = vim.fn.expand("~/dotfiles") })',
+  --         },
+  --         {
+  --           icon = '󰈆  ',
+  --           desc = 'Quit',
+  --           key = 'q',
+  --           key_format = ' %s',
+  --           action = 'quit',
+  --         },
+  --       },
+  --       footer = {},
+  --     },
+  --   }
+  -- end,
+  -- dependencies = { 'nvim-tree/nvim-web-devicons' },
 }
